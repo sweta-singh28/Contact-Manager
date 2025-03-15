20d4a19e534119a914ad01e6f55f6cf123bb7db4
sweta-singh28 <swetasinghgs28@gmail.com>
2025-03-15 23:06:16 +0530

    changes

--- client/src/Components/ContactDetails.jsx
+++ client/src/Components/ContactDetails.jsx
@@ -10,25 +10,28 @@ import {
   ModalHeader,
   ModalBody,
 } from "reactstrap";
-import { getContactById} from "../api/contacts";
+import { getContactById } from "../api/contacts";
 import ContactForm from "./ContactForm"; // Import the ContactForm component
 import "./../css/ContactDetails.css";
+import { Trash2, Pencil } from "lucide-react"; // Import both Trash2 and Pencil icons
 
 const ContactDetails = () => {
   const { id } = useParams(); // Get the contact ID from URL parameters
   const [contact, setContact] = useState(null); // Store contact details
   const [modal, setModal] = useState(false); // State to handle modal visibility
+
   const fetchContact = async () => {
     const data = await getContactById(id);
     const contact = data;
     setContact(contact);
   };
+
   useEffect(() => {
-    
     fetchContact();
   }, [id]);
 
-  const toggleModal = () => {setModal(!modal)
+  const toggleModal = () => {
+    setModal(!modal);
     fetchContact();
   }; // Function to toggle the modal
 
@@ -42,15 +45,24 @@ const ContactDetails = () => {
             <CardText>Email: {contact.Email}</CardText>
             <CardText>Gender: {contact.Gender}</CardText>
             <CardText>Location: {contact.Location}</CardText>
-            <Button color="primary" onClick={toggleModal}>
-              Edit
+            {/* Add Trash2 icon for deleting */}
+            <Button color="danger" className="ml-2">
+              <Trash2 size={20} /> 
+            </Button>
+            {/* Add Pencil icon for editing */}
+            <Button color="primary" className="ml-2" onClick={toggleModal}>
+              <Pencil size={20} /> 
             </Button>
 
             {/* Modal for ContactForm */}
             <Modal isOpen={modal} toggle={toggleModal}>
               <ModalHeader toggle={toggleModal}>Edit Contact</ModalHeader>
               <ModalBody>
-                <ContactForm contact={contact} closeModal={toggleModal} isEditing={true}/>
+                <ContactForm
+                  contact={contact}
+                  closeModal={toggleModal}
+                  isEditing={true}
+                />
               </ModalBody>
             </Modal>
           </>
